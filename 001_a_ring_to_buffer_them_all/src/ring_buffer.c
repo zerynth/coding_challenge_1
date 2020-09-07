6493a74a39175a2d335253ba61c397bdc537edbf
Milos Vidojevic <m.vidojevic@kci-system.com>
2020-09-07 18:28:27 +0200

    Ring buffer implementaion finished. Documented. Tested.

--- 001_a_ring_to_buffer_them_all/src/ring_buffer.c
+++ 001_a_ring_to_buffer_them_all/src/ring_buffer.c
@@ -7,46 +7,219 @@
  *
  * @copyright Copyright (c) 2020 - GPLv3
  *
+ * Modified version of https://github.com/RIOT-OS/RIOT/tree/master/sys/tsrb.
+ * Author: Kaspar Schleiser
+ * Copyright (C) 2015 Kaspar Schleiser <kaspar@schleiser.de>
  */
 
 #include "ring_buffer.h"
+#include <stddef.h>
+
+#define ASSERT(x)
+
+#if ( RING_BUFFER_SINGLE_PRODUCER_CONSUMER == 0 )
+#if ( RING_BUFFER_MULTIPLE_ISRS != 0 ) && ( RING_BUFFER_MULTIPLE_THREADS != 0 )
+#error "Chose only one of available options to ensure thread safety."
+#endif
+#endif
+
+#if ( RING_BUFFER_SINGLE_PRODUCER_CONSUMER == 0 )
+#if ( RING_BUFFER_MULTIPLE_ISRS != 0 )
+
+/*
+    Declare all necessary variables which will later allow atomic execution of
+    the code which will be placed between @ref TURN_ATOMIC_ON and
+    @ref TURN_ATOMIC_OFF.
+*/
+#define DEFINE_ATOMIC( )
+
+/*
+    This macro should enable atomic execution of the code which is later
+    disabled by @ref TURN_ATOMIC_OFF macro. In general interrups should be
+    disable in this macro - as fastest as possible.
+*/
+#define TURN_ATOMIC_ON( )
+
+/*
+    This macro should disable atomic execution of the code which is previously
+    enable by @ref TURN_ATOMIC_ON macro. In general interrups should be
+    enabled in this macro - as fastest as possible.
+*/
+#define TURN_ATOMIC_OFF( )
+
+/*
+    Comment out nex line of the code if you implemented macros which allows
+    atomic execution of the code.
+*/
+#warning "Atomic execution macros not defined"
+#endif
+#if ( RING_BUFFER_MULTIPLE_THREADS != 0 )
+
+#define DEFINE_ATOMIC( )
+
+#define TURN_ATOMIC_ON( )                                                       \
+    do                                                                          \
+    {                                                                           \
+        if ( NULL != ctx->lock ) ctx->lock( ctx );                              \
+    }                                                                           \
+    while( 0 )
+
+#define TURN_ATOMIC_OFF( )                                                      \
+    do                                                                          \
+    {                                                                           \
+        if ( NULL != ctx->unlock ) ctx->unlock( ctx );                          \
+    }                                                                           \
+    while( 0 )
+#endif
+#else
+#define DEFINE_ATOMIC( )
+#define TURN_ATOMIC_ON( )
+#define TURN_ATOMIC_OFF( )
+#endif
+
+
+// ---------------------------------------------- PRIVATE FUNCTION DECLARATIONS
+
+static inline void
+_push( rb_ctx_t * ctx, rb_node_t n )
+{
+    ctx->buf[ctx->writes++ & (ctx->size - 1)] = n;
+}
+
+static inline void
+_pop( rb_ctx_t * ctx, rb_node_t * n )
+{
+    * n = ctx->buf[ctx->reads++ & (ctx->size - 1)];
+}
 
 // --------------------------------------------------------- EXPORTED FUNCTIONS
 
 rb_result_t
-rb_initialize( rb_ctx_t * ctx, rb_node_t buffer, rb_size_t size )
+rb_initialize( rb_ctx_t * ctx, rb_node_t * buffer, rb_size_t size )
 {
+    ASSERT( ctx != NULL );
+    ASSERT( buffer != NULL );
 
+    /*
+        Make sure bufsize is a power of two.
+        http://www.exploringbinary.com/ten-ways-to-check-if-an-integer-is-a-power-of-two-in-c/
+    */
+    if ((size != 0) && ((size & (~size + 1)) != size))
+    {
+        return RB_ERROR;
+    }
+
+    ctx->buf = buffer;
+    ctx->size = size;
+    ctx->reads = 0;
+    ctx->writes = 0;
+
+#if ( RING_BUFFER_SINGLE_PRODUCER_CONSUMER == 0 )
+#if ( RING_BUFFER_MULTIPLE_THREADS != 0 )
+
+    ctx->lock = NULL;
+    ctx->unlock = NULL;
+
+#endif
+#endif
+
+    return RB_OK;
 }
 
-rb_size_t
-rb_available( rb_ctx_t * ctx )
-{
+#if ( RING_BUFFER_SINGLE_PRODUCER_CONSUMER == 0 )
+#if ( RING_BUFFER_MULTIPLE_THREADS != 0 )
 
+rb_result_t
+rb_attach_mutex_calls( rb_ctx_t * ctx, rb_mutex_fp_t locker,
+                       rb_mutex_fp_t unlocker )
+{
+    ASSERT( ctx != NULL );
+    ASSERT( locker != NULL );
+    ASSERT( unlocker != NULL );
+
+    ctx->lock = locker;
+    ctx->unlock = unlocker;
+}
+
+#endif
+#endif
+
+rb_result_t
+rb_get( rb_ctx_t * ctx, rb_node_t * data )
+{
+    DEFINE_ATOMIC( );
+    TURN_ATOMIC_ON( );
+
+    if ( !rb_empty( ctx ) )
+    {
+        _pop( ctx, data );
+    }
+    else
+    {
+        TURN_ATOMIC_OFF( );
+        return RB_ERROR;
+    }
+
+    TURN_ATOMIC_OFF( );
+
+    return RB_OK;
 }
 
 rb_result_t
 rb_put( rb_ctx_t * ctx, rb_node_t data )
 {
+    DEFINE_ATOMIC( );
+    TURN_ATOMIC_ON( );
 
-}
+    if ( !rb_full( ctx ) )
+    {
+        _push( ctx, data );
+    }
+    else
+    {
+        TURN_ATOMIC_OFF( );
+        return RB_ERROR;
+    }
 
-rb_result_t
-rb_get( rb_ctx_t * ctx, rb_node_t * data )
-{
+    TURN_ATOMIC_OFF( );
 
+    return RB_OK;
 }
 
 rb_size_t
 rb_read( rb_ctx_t * ctx, rb_node_t * data, rb_size_t size )
 {
+    rb_size_t idx = 0;
 
+    DEFINE_ATOMIC( );
+    TURN_ATOMIC_ON( );
+
+    while ( !rb_empty( ctx ) && ( idx < size ) )
+    {
+        _pop( ctx, &data[idx++] );
+    }
+
+    TURN_ATOMIC_OFF( );
+
+    return idx;
 }
 
 rb_size_t
 rb_write( rb_ctx_t * ctx, rb_node_t * data, rb_size_t size )
 {
+    rb_size_t idx = 0;
 
+    DEFINE_ATOMIC( );
+    TURN_ATOMIC_ON( );
+
+    while ( !rb_full( ctx ) && ( idx < size ) )
+    {
+        _push( ctx, data[idx++] );
+    }
+
+    TURN_ATOMIC_OFF( );
+
+    return idx;
 }
 
 // ---------------------------------------------------------------- END OF FILE
--- 001_a_ring_to_buffer_them_all/src/ring_buffer_test_main.c
+++ 001_a_ring_to_buffer_them_all/src/ring_buffer_test_main.c
@@ -10,10 +10,103 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 #include "ring_buffer.h"
+
+#define STORAGE_1_SIZE 22
+#define STORAGE_2_SIZE 512
+#define STORAGE_3_SIZE 256
+
+rb_node_t storage_1[STORAGE_1_SIZE];
+rb_node_t storage_2[STORAGE_2_SIZE];
+rb_node_t storage_3[STORAGE_3_SIZE];
+
+rb_ctx_t mybuf_1;
+rb_ctx_t mybuf_2;
+rb_ctx_t mybuf_3;
+
+// -------------------
+
+uint8_t temp_array[130] = "1234567890123456789012345678901234567890"
+                          "assdasdadadasdadadaddadadasdasdadasdsada"
+                          "assdasdadad\nsdadadaddadadasdasdadasdsada"
+                          "123456789x";
+uint32_t temp_res;
+
+// ------------------
+
+uint8_t tmp_buf[STORAGE_2_SIZE + 1];
+uint8_t tmp_idx;
+
+// --------------------
 
 int main( int argc, char ** argv )
 {
+    if ( RB_OK != rb_initialize( &mybuf_1, storage_1, STORAGE_1_SIZE ) )
+    {
+        printf( "\r\nBuffer 1 init err" );
+    }
+    if ( RB_OK != rb_initialize( &mybuf_2, storage_2, STORAGE_2_SIZE ) )
+    {
+        printf( "\r\nBuffer 2 init err" );
+    }
+    if ( RB_OK != rb_initialize( &mybuf_3, storage_3, STORAGE_3_SIZE ) )
+    {
+        printf( "\r\nBuffer 3 init err" );
+    }
+
+    for ( ; ; )
+    {
+        if ( 0 == ( temp_res = rb_write( &mybuf_3, temp_array, 130 ) ) )
+        {
+            printf( "\r\nCan't write no more" );
+            break;
+        }
+        else
+        {
+            printf( "\r\nBuffer 3 bytes written : %d", temp_res );
+        }
+    }
+
+    // ----------- Exam from From readme.
+
+    printf( "\r\nExam..." );
+    memset( tmp_buf, 0, sizeof( tmp_buf ) );
+    tmp_idx = 0;
+
+    for ( ; ; )
+    {
+        //  Poll Ring buffer 2.
+
+        if ( RB_OK == rb_get( &mybuf_2, &tmp_buf[ tmp_idx ] ) )
+        {
+            if ( '\n' == tmp_buf[ tmp_idx ] )
+            {
+                //  Print the content and reset temporary storage.
+
+                printf( "%s", tmp_buf );
+                memset( tmp_buf, 0, sizeof( tmp_buf ) );
+                tmp_idx = 0;
+            }
+            else
+            {
+                tmp_idx++;
+            }
+        }
+    }
 
     return 0;
+}
+
+void __SOME_UART_ISR ( void )
+{
+    uint8_t tmp;
+
+    //  Store content of the UART RX register to the tmp variable.
+    //  ...
+
+    rb_put( &mybuf_2, tmp );
+
+    //  Clean UART interrupt flag.
+    //  ...
 }
